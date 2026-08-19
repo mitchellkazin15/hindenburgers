51c4976bfb4bbfc64ed7c2f4c252b5fd9da535a8
Mitch Kazin <mitchell.kazin15@gmail.com>
2026-08-18 20:49:03 -0600

    added ripple and chromatic effect to lsd

--- characters/drug_manager.gd
+++ characters/drug_manager.gd
@@ -8,9 +8,9 @@ var shader : ShaderMaterial
 var fractal_noise_texture : NoiseTexture2D
 var fractal_noise : FastNoiseLite
 
-var noise_update_interval := 0.25  # regenerate ~10x/sec instead of 60x/sec
+var noise_update_interval := 0.05
 var _accum := 0.0
-var noise_updates = 0.0
+var total_accum = 0.0
 
 
 func _ready() -> void:
@@ -38,7 +38,7 @@ func apply_drug_character_effects(stat_adders, stat_multipliers, effect_duration
 	character_stats.register_all_temp_multipliers(stat_multipliers, effect_duration)
 
 
-func _on_visual_stat_update(stat_name):
+func _on_visual_stat_update(stat_name : String):
 	var curr_stat = visual_stats._get_modified_stat(stat_name)
 	if stat_name == "saturation_color":
 		# Color needs to be handled differently
@@ -46,17 +46,27 @@ func _on_visual_stat_update(stat_name):
 		var int_color = int(float_color)
 		shader.set_shader_parameter(stat_name, Color(int_color))
 		return
-	shader.set_shader_parameter(stat_name, curr_stat)
+	elif stat_name in DrugVisualEffectStatManager.SHADER_PARAMS_NAMES:
+		shader.set_shader_parameter(stat_name, curr_stat)
 
 
 func _process(delta):
 	_accum += delta
+	total_accum += delta
 	if _accum >= noise_update_interval:
 		_accum -= noise_update_interval
-		noise_updates += 1.0
 		_update_noise_params()
 
 func _update_noise_params():
-	fractal_noise.fractal_ping_pong_strength = 6.0 + 4.0 * sin(TAU * noise_updates / 200.0)
-	fractal_noise.fractal_lacunarity = 2 + 1.0 * sin(TAU * noise_updates / 100.0)
-	fractal_noise.fractal_gain = 0.5 + 0.4 * sin(TAU * noise_updates / 500.0)
+	fractal_noise.fractal_ping_pong_strength = (6.0 + 
+		visual_stats.get_current_fractal_ping_pong_strength_amplitude() * 
+		sin(TAU * total_accum * visual_stats.get_current_fractal_ping_pong_strength_frequency())
+	)
+	fractal_noise.fractal_lacunarity = (5 + 
+		visual_stats.get_current_fractal_lacunarity_amplitude() * 
+		sin(TAU * total_accum * visual_stats.get_current_fractal_lacunarity_frequency())
+	)
+	fractal_noise.fractal_gain = (0.7 + 
+		visual_stats.get_current_fractal_gain_amplitude() * 
+		sin(TAU * total_accum * visual_stats.get_current_fractal_gain_frequency())
+	)
--- stats/drug_visual_effect_stat_manager.gd
+++ stats/drug_visual_effect_stat_manager.gd
@@ -13,8 +13,37 @@ const EXAMPLE_DICT = { # Do NOT update values in this script and expect stats to
 	"brightness": 0.0,
 	"contrast": 0.0,
 	"saturation": 0.0,
-	"saturation_color": 0.0
-} 
+	"saturation_color": 0.0,
+	"fractal_noise_distortion": 0.0,
+	"fractal_noise_iterations": 0.0,
+	"fractal_noise_overlay_strength": 0.0,
+	"fractal_chromatic_shift_strength": 0.0,
+	"fractal_ping_pong_strength_amplitude": 0.0,
+	"fractal_lacunarity_amplitude": 0.0,
+	"fractal_gain_amplitude": 0.0,
+	"fractal_ping_pong_strength_frequency": 0.0,
+	"fractal_lacunarity_frequency": 0.0,
+	"fractal_gain_frequency": 0.0
+}
+
+const SHADER_PARAMS_NAMES = [
+	"x_wiggle_amp",
+	"x_wiggle_screen_freq",
+	"x_wiggle_time_freq",
+	"y_wiggle_amp",
+	"y_wiggle_screen_freq",
+	"y_wiggle_time_freq",
+	"blur_iterations",
+	"blur_power",
+	"brightness",
+	"contrast",
+	"saturation",
+	"saturation_color",
+	"fractal_noise_distortion",
+	"fractal_noise_iterations",
+	"fractal_noise_overlay_strength",
+	"fractal_chromatic_shift_strength"
+]
 
 ## Dict of stat_name -> base val.
 @export var base_stats = EXAMPLE_DICT
@@ -23,3 +52,27 @@ const EXAMPLE_DICT = { # Do NOT update values in this script and expect stats to
 func _ready():
 	_base_stats = base_stats
 	super._ready()
+
+
+func get_current_fractal_ping_pong_strength_amplitude():
+	return _get_modified_stat("fractal_ping_pong_strength_amplitude")
+
+
+func get_current_fractal_lacunarity_amplitude():
+	return _get_modified_stat("fractal_lacunarity_amplitude")
+
+
+func get_current_fractal_gain_amplitude():
+	return _get_modified_stat("fractal_gain_amplitude")
+
+
+func get_current_fractal_ping_pong_strength_frequency():
+	return _get_modified_stat("fractal_ping_pong_strength_frequency")
+
+
+func get_current_fractal_lacunarity_frequency():
+	return _get_modified_stat("fractal_lacunarity_frequency")
+
+
+func get_current_fractal_gain_frequency():
+	return _get_modified_stat("fractal_gain_frequency")
